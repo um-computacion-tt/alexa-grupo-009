f4252766dc5d27f9307deb2a8f6f1976fac371c4
MelinaMartin <mem.martin@alumno.um.edu.ar>
2022-10-24 12:12:04 -0300

    intento2

--- proyecto.py
+++ proyecto.py
@@ -1,18 +1,36 @@
 import speech_recognition as sr
 import pyttsx3
+import pywhatkit
 
 listener = sr.Recognizer()
 engine = pyttsx3.init()
-engine.say('I am your Alexa') #Para q hable cuando empieza el programa
-engine.say('what can i do for you?')
-engine.runAndWait()
-try:   # te deja ver errores en el codigo
-    with sr.Microphone() as source:
-        print('listening...') #para saber cuando te esta escuchando
-        voice = listener.listen(source) #tenemos q hablar fuerte para q nos escuche
-        command = listener.recognize_google(voice) #para que google te tire el texto    
-        command = command.lower()
-        if "alexa" in command:    #para q solo conteste en el caso q digan alexa
-            print(command)
-except:   #te deja arreglar el error
-    pass 
+voices = engine.getProperty('voices') #para cambiarle la voz
+engine.setProperty('voice', voices[1].id) #tiene 2 voces posibles, 0 es masculina y 1 es femenina   
+
+def talk(text):
+    engine.say(text)
+    engine.runAndWait()
+
+def take_command():
+    try:   # te deja ver errores en el codigo
+        with sr.Microphone() as source:
+            print('listening...') #para saber cuando te esta escuchando
+            voice = listener.listen(source) #tenemos q hablar fuerte para q nos escuche
+            command = listener.recognize_google(voice) #para que google te tire el texto    
+            command = command.lower()
+            if "alexa" in command:    #para q solo conteste en el caso q digan alexa
+                command = command.replace('alexa', '') #saca alexa del texto, para q no lo diga
+                print(command)
+    except:   #te deja arreglar el error
+        pass 
+    return command
+
+def run_alexa(): #para q ponga musica
+    command = take_command()
+    print(command)
+    if 'play' in command: 
+        song = command.replace('play', '')
+        talk('playing ' + song)
+        pywhatkit.playonyt(song) #con esto tendria q poer escuchar musica en youtube
+
+run_alexa()      
