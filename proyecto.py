5dee5de3f077d527a97ffbf4242816ef9229e26e
MelinaMartin <mem.martin@alumno.um.edu.ar>
2022-11-21 16:25:41 -0300

    proyecto terminado

--- proyecto.py
+++ proyecto.py
@@ -7,49 +7,49 @@ import pyjokes
 
 listener = sr.Recognizer()
 engine = pyttsx3.init()
-voices = engine.getProperty('voices') #para cambiarle la voz
-engine.setProperty('voice', voices[1].id) #tiene 2 voces posibles, 0 es masculina y 1 es femenina   
+voices = engine.getProperty('voices') 
+engine.setProperty('voice', voices[1].id) 
 
 def talk(text):
     engine.say(text)
     engine.runAndWait()
 
 def take_command():
-    try:   # te deja ver errores en el codigo
+    try: 
         with sr.Microphone() as source:
-            print('listening...') #para saber cuando te esta escuchando
-            voice = listener.listen(source) #tenemos q hablar fuerte para q nos escuche
-            command = listener.recognize_google(voice) #para que google te tire el texto    
+            print('listening...') 
+            voice = listener.listen(source)
+            command = listener.recognize_google(voice) 
             command = command.lower()
-            if "alexa" in command:    #para q solo conteste en el caso q digan alexa
-                command = command.replace('alexa', '') #saca alexa del texto, para q no lo diga
+            if "alexa" in command:    
+                command = command.replace('alexa', '') 
                 print(command)
-    except:   #te deja arreglar el error
+    except:   
         pass 
     return command
 
-def run_alexa(): #para q ponga musica
+def run_alexa(): 
     command = take_command()
     print(command)
-    if 'play' in command: 
+    if 'play' in command:                                           
         song = command.replace('play', '')
         talk('playing ' + song)
-        pywhatkit.playonyt(song) #con esto escucho musica en youtube
-    elif 'time' in command:
-            time = datetime.datetime.now().strftime('%H:%M') #en 24hs
+        pywhatkit.playonyt(song)
+    elif 'time' in command:                                          
+            time = datetime.datetime.now().strftime('%H:%M') 
             print(time)
             talk('Current time is ' + time)
-            time = datetime.datetime.now().strftime('%I:%M %p') #en 12hs
+            time = datetime.datetime.now().strftime('%I:%M %p') 
             print(time)
             talk('Current time is ' + time)
-    elif 'who is' in command: #para buscar en wikipedia
+    elif 'who is' in command: 
             person = command.replace('who is ', '')
             info = wikipedia.summary(person, 1)
             print(info)
             talk(info)
     elif 'joke' in command:
         talk(pyjokes.get_joke())
-    else: #por si no te escucha o no te entiende
+    else: 
         talk('Please say the command again')
 
 while True:
