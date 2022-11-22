069d4b816ebeb1f484d8a44eef0787042423195f
MelinaMartin <mem.martin@alumno.um.edu.ar>
2022-11-22 16:06:51 -0300

    proyecto terminado

--- proyecto.py
+++ proyecto.py
@@ -9,6 +9,7 @@ listener = sr.Recognizer()
 engine = pyttsx3.init()
 voices = engine.getProperty('voices') 
 engine.setProperty('voice', voices[1].id) 
+recalling = []
 
 def talk(text):
     engine.say(text)
@@ -17,13 +18,13 @@ def talk(text):
 def take_command():
     try: 
         with sr.Microphone() as source:
-            print('listening...') 
+            talk('listening...') 
+            print('listening...')
             voice = listener.listen(source)
             command = listener.recognize_google(voice) 
             command = command.lower()
             if "alexa" in command:    
                 command = command.replace('alexa', '') 
-                print(command)
     except:   
         pass 
     return command
@@ -31,6 +32,8 @@ def take_command():
 def run_alexa(): 
     command = take_command()
     print(command)
+    recalling.append(command)
+
     if 'play' in command:                                           
         song = command.replace('play', '')
         talk('playing ' + song)
@@ -39,9 +42,9 @@ def run_alexa():
             time = datetime.datetime.now().strftime('%H:%M') 
             print(time)
             talk('Current time is ' + time)
-            time = datetime.datetime.now().strftime('%I:%M %p') 
-            print(time)
-            talk('Current time is ' + time)
+            #time = datetime.datetime.now().strftime('%I:%M %p') 
+            #print(time)
+            #talk('Current time is ' + time)
     elif 'who is' in command: 
             person = command.replace('who is ', '')
             info = wikipedia.summary(person, 1)
@@ -49,8 +52,40 @@ def run_alexa():
             talk(info)
     elif 'joke' in command:
         talk(pyjokes.get_joke())
+    elif 'who are you' in command:
+        talk('i am your personal virtual assistand')
+    elif 'where is' in command:
+            place = command.replace ('where is', '')
+            info = wikipedia.summary(place,1)
+            print(info)
+            talk(info)
+    elif 'goodbye' in command:
+        pass
+    elif 'function' in command:
+        print('i have the following function: ')
+        talk('i have the following function: ')
+        print('for every function to work you have to say alexa first, and then the command')
+        talk('for every function to work you have to say alexa first, and then the command')
+        print('for looking videos in youtube you say the command "play" ')
+        talk('for looking videos in youtube you say the command "play" ')
+        print('for the time you say the command "time" ')
+        talk('for the time you say the command "time" ')
+        print('for looking a short description of a person you say the command "who is" ')
+        talk('for looking a short description of a person you say the command "who is" ')
+        print('for looking for a place, like a country or a city you say the command "where is" ')
+        talk('for looking for a place, like a country or a city you say the command "where is" ')
+        print('if you want me to tell you a joke you say the command "joke" ')
+        talk('if you want me to tell you a joke you say the command "joke" ')
+        print('if you want to know who i am you say the command "who are you" ')
+        talk('if you want to know who i am you say the command "who are you" ')
+        print('if you do not want to continue talking to me you say the command "goodbye" ')
+        talk('if you do not want to continue talking to me you say the command "goodbye" ')        
     else: 
-        talk('Please say the command again')
+        talk('Please say the command again') 
 
 while True:
-    run_alexa()      
+    run_alexa()
+    if 'goodbye' in recalling[-1]:
+        break
+    elif 'play' in recalling[-1]:
+        break
